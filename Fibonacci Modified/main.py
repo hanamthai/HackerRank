587fa4124cf1fe584b1f399478e89f5d04a83a32
ThaiNamHa-windows <hanamthai02@gmail.com>
2023-05-05 13:58:32 +0700

    Fibonacci Modified

---
+++ Fibonacci Modified/main.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'fibonacciModified' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER t1
+#  2. INTEGER t2
+#  3. INTEGER n
+#
+
+def fibonacciModified(t1, t2, n):
+    # Write your code here
+    if n == 2:
+        return t2
+    n = n - 1
+    return fibonacciModified(t2,t1 + t2**2,n)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    t1 = int(first_multiple_input[0])
+
+    t2 = int(first_multiple_input[1])
+
+    n = int(first_multiple_input[2])
+
+    result = fibonacciModified(t1, t2, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
